ce63d3ae0241190f7feac74e1b812a016ea213f6
AbodSinan96 <abodsinan96@gmail.com>
2022-12-28 15:01:31 +0800

    Added product services for sales

--- product/views.py
+++ product/views.py
@@ -9,6 +9,7 @@ from .serializers import ProductSerializer
 from store.models import Store
 from material.models import MaterialQuantity, Material
 import json
+from product.services.sale import process_sales
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer    
@@ -67,32 +68,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     @action(detail = False, methods = ["post"])
     def sale(self, request):
         sales = [json.loads(sale.replace('\'', '\"')) for sale in request.data.getlist('sale')]
-        track_material_quantity = {}
-
-        for sale in sales:
-            product = sale['product']
-            quantity = sale['quantity']
-            materials = Product.objects.get(pk = product).material_entries.all()
-            for material in materials:
-                mq = MaterialQuantity.objects.get(material = material, product = product)
-                
-                ## Check is it able to sale
-                # If current capacity is less than the required quantity 
-                # Response an error message
-                if material.current_capacity - (mq.quantity * quantity) < 0:
-                    return Response({"error_messages": "Please enter a valid sale data"})
-
-                # Initialize the material to be tracked
-                if material not in track_material_quantity:
-                    track_material_quantity[material.pk] = 0
-                # Track the quantity for each material that will be deducted later
-                track_material_quantity[material.pk] += mq.quantity * quantity
-
-        ## Product sold successfully and have to deduct the material that have been sold
-        for material_id in track_material_quantity:
-            m = Material.objects.get(pk = material_id)
-            m.current_capacity -= track_material_quantity[material_id]
-            m.save()
+        process_sales(sales)
 
         return Response({"message": "Sale Successfully"})
 
